49e194538af9a209b10dc302875f38c9f84097e5
pgfisico <pgfisico.dev@gmail.com>
2020-05-18 17:31:19 -0600

    Add domain_name output (#6)
    Closes #5

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
+output "domain_name" {
+  description = "The domain name"
+  value       = aws_route53_zone.domain.name
+}
+
 output "zone_id" {
-  description = "The Route53 Zone ID for the intimitrons.ca domain"
+  description = "The Route53 Zone ID for domain_name"
   value       = aws_route53_zone.domain.zone_id
 }
 
 output "name_servers" {
-  description = "The assigned name servers for the intimitrons.ca domain"
+  description = "The assigned name servers for domain_name"
   value       = aws_route53_zone.domain.name_servers
 }

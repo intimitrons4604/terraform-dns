17271ea466d578fe24238198b967577a4f482754
pgfisico <pgfisico.dev@gmail.com>
2021-01-05 20:01:10 -0700

    Add G Suite verification record (#16)

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_route53_zone" "domain" {
     "trons:terraform"   = "true"
   }
 }
+
+resource "aws_route53_record" "google_g_suite_verification" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = 300
+  records = ["google-site-verification=BsdsEfumykT04H9iMMz-dmWPkDgmknGNPtq0Moq_diw"]
+}
